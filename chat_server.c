ecaa9f9d7260cd3511415ccafc0b64c68391391b
tgiarrat <tgiarrat@calpoly.edu>
2017-04-25 17:25:52 -0700

    debug

--- chat_server.c
+++ chat_server.c
@@ -196,7 +196,7 @@ struct clientNode  newClientConnection(int serverSocket,struct clientNode *head,
 		sendValidHandle(clientSocket);
 	}
 
-	printf("Node to add has handle %s\n", node.handle);
+	printf("Node to add has handle %s\n", nodePtr.handle);
 	toAdd = malloc(sizeof(struct clientNode));
 	memcpy(toAdd, &nodePtr, sizeof(struct clientNode));
 	return nodePtr; 
