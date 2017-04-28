d5f9f60d1a12df01874bd0f039f40ed347dd3323
tgiarrat <tgiarrat@calpoly.edu>
2017-04-28 13:47:43 -0700

    formatting

--- chat_server.h
+++ chat_server.h
@@ -1,3 +1,6 @@
+
+#include "networks.h"
+
 #define MAXBUF 1024
 #define DEBUG_FLAG 1
 #define MAX_HANDLE_LEN 250
@@ -11,7 +14,7 @@ struct clientNode{
     struct clientNode *next;
 }__attribute__((packed));
 
-int sendPacket(int socket, char *packet, chat_header cheader); 
+int sendPacket(int socket, char *packet, struct chat_header cheader); 
 int getSocket(char *handle, struct clientNode *head);
 int messageRecieved(char *recieved, struct chat_header cheader, struct clientNode *head);
 int addClient(struct clientNode **head, char *handle, int handleLen, int clientSocket);
