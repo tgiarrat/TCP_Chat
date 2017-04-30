15bb558a04434fa1610043f76063cc17f0541b04
tgiarrat <tgiarrat@calpoly.edu>
2017-04-30 15:39:40 -0700

    changes

--- chat_client.c
+++ chat_client.c
@@ -260,7 +260,7 @@ int block(char *textBuffer, struct blockedHandles **blockedHandles) {
 	printf("\n");
 	
 	invalidHandle = strtok(textBuffer, " ");
-	printf("Here is the invalid handle... %s", invalidHandle);
+	printf("Here is the invalid handle... ");
 	if (checkBlocked(invalidHandle, *blockedHandles) == 1){
 		printf("Block failed, handle %s is already blocked.\n", invalidHandle);
 		return 1;
