995413d0665cc0c7d39fb40984d4c4d3223aec01
tgiarrat <tgiarrat@calpoly.edu>
2017-05-01 13:47:23 -0700

    :)

--- chat_client.c
+++ chat_client.c
@@ -103,8 +103,8 @@ int serverActivity(int socketNum, struct blockedHandles *blockedHandles) {
 		listRecieved(buf + sizeof(struct chat_header), cheader, socketNum);
 	}
 	else if(byteFlag == 9) {
-		printf("got here at least\n");
-		//exitACK(blockedHandles);
+		//printf("got here at least\n");
+		exitACK(blockedHandles);
 		return 0;
 	}
 	printf("$:");
